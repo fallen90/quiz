6b13a3fab45985975c46f2b3111ce407dc17b0d5
fallen90 <slytherin9090@gmail.com>
2014-08-26 10:31:39 +0800

    Converted to Yeoman Project

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name quizApp
+ * @description
+ * # quizApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('quizApp', [
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
